d0e7796f1095507be36d1772ebcc4068871a7903
DJ Stewart <deshastewart1992@gmail.com>
2020-08-09 23:29:19 -0700

    Updates

---
+++ precache-manifest.7ebca65130718389a9f3db6a6e001fc6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e109ee7357f02bde3565f8e8a2d5bc2",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "71a2ab5e0f6bfd374637",
+    "url": "/todo/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "19ab149998ec22c09a03",
+    "url": "/todo/static/js/2.ebe2f1dd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo/static/js/2.ebe2f1dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71a2ab5e0f6bfd374637",
+    "url": "/todo/static/js/main.581e8b47.chunk.js"
+  },
+  {
+    "revision": "a8c0d0ef7646a03dcdd7",
+    "url": "/todo/static/js/runtime-main.1a0ec889.js"
+  }
+]);
